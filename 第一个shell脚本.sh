a6d869a1eed02f42a1fe1d3628cf7aa309a02e21
test <test@test.com>
2023-02-16 22:08:31 +0800

    2

--- 第一个shell脚本.sh
+++ 第一个shell脚本.sh
@@ -3,3 +3,15 @@
 #在用户的Documents中新建一个test1文件夹
 #在test1文件夹中创建myTry文件
 #在myTry文件中写 echo "hello world"
+#安装了net-tools
+#:<<EOF
+while true
+do
+    /sbin/ifconfig ens33 down;
+    /sbin/ifconfig ens33 hw ether 00:0C:29:BB:21:E8
+    #/sbin/ifconfig ens33 10.96.0.150
+    /sbin/ifconfig ens33 up;
+    sleep 10;
+    echo 'haha23';
+done
+#EOF
