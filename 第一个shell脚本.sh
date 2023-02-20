b2ecd8b09c84627e726cd40aa86c927c3dcf4cd0
test <test@test.com>
2023-02-20 14:43:28 +0800

    3

--- 第一个shell脚本.sh
+++ 第一个shell脚本.sh
@@ -7,11 +7,18 @@
 #:<<EOF
 while true
 do
-    /sbin/ifconfig ens33 down;
-    /sbin/ifconfig ens33 hw ether 00:0C:29:BB:21:E8
-    #/sbin/ifconfig ens33 10.96.0.150
-    /sbin/ifconfig ens33 up;
+    ########################################
+    #适用于17.0之前修改该mac
+    #/sbin/ifconfig ens33 down;
+    #/sbin/ifconfig ens33 hw ether 00:0C:29:BB:21:E8
+    #/sbin/ifconfig ens33 up;
+    #######################################
+    #适用于20.4修改mac
+    macchanger -r ens33
+    # sudo macchanger –m c2:43:bc:1c:62:01 enp0s3 手动修改mac
+    # sudo macchanger –p enp0s3 还原原始mac
     sleep 10;
-    echo 'haha23';
+    echo "hello world"
+    #/sbin/ifconfig ens33 10.96.0.150
 done
 #EOF
